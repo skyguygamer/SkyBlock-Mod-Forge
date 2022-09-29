71ab74d3218be1171d04112d7f2b3ee87647e0e7
BuildTools <unconfigured@null.spigotmc.org>
2022-09-28 20:07:50 -0400

    Test

--- src/main/java/net/skyguygamer/sbmod/SbMod.java
+++ src/main/java/net/skyguygamer/sbmod/SbMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//test
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SbMod.MOD_ID)
 public class SbMod
